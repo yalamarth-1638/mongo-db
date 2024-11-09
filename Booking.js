6b51864c62e9a5a2c1da0d1283b328dd1f8c4f7e
yalamarth-1638 <yalamarthimaheshwari@gmail.com>
2024-11-09 20:46:24 +0530

    Create Booking.js

---
+++ Booking.js
@@ -0,0 +1,10 @@
+ const mongoose = require('mongoose');
+const bookingSchema = new mongoose.Schema({
+  hotelId: { type: mongoose.Schema.Types.ObjectId, ref: 'Hotel', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  checkInDate: { type: Date, required: true },
+  checkOutDate: { type: Date, required: true },
+  totalAmount: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
